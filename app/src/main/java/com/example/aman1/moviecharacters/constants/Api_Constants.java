11b0cb2965d0e732ad5679562be1d86e179280ae
amanbahataTAE <amanbahata.test@gmail.com>
2017-12-06 10:08:55 +0000

    Adds product flavours

--- app/src/main/java/com/example/aman1/moviecharacters/constants/Api_Constants.java
+++ app/src/main/java/com/example/aman1/moviecharacters/constants/Api_Constants.java
@@ -1,10 +1,13 @@
 package com.example.aman1.moviecharacters.constants;
 
+import com.example.aman1.moviecharacters.BuildConfig;
+
 /**
  * Created by aman1 on 05/12/2017.
  */
 
 public class Api_Constants {
-    public static final String URL = "http://api.duckduckgo.com/?q=family+guy+characters&format=json";
+
+    public static final String URL = BuildConfig.API_URL;
 
 }
